5ad48239c86a9b28d7d0256d3388081b599fd60c
Pierre Vidal <p.terkal@gmail.com>
2016-10-22 14:59:06 +0200

    Fixed scripts init and spawn accordingly to current Lua interface.

--- init.lua
+++ init.lua
@@ -1,58 +1,61 @@
 #!/usr/bin/lua
 
 setverbose()
-verboseinfo("Lua Init...")
+-- verboseinfo("Lua Init...")
+verbose("Lua Init...")
 
-server_addtile(Server, new_tile("floor", "old paved floor", string.byte('.')))
-server_addtile(Server, new_tile("floor drawn", "old paved floor with chalk glyph", string.byte(';')))
-server_addtile(Server, new_tile("portal", "magic portal", string.byte('^')))
-server_addtile(Server, new_tile("door", "wooden door, closed", string.byte('|'), false))
-server_addtile(Server, new_tile("door_open", "wooden door, opened", string.byte('_')))
-server_addtile(Server, new_tile("grave_a", "stone cross grave", string.byte('+'), false))
-server_addtile(Server, new_tile("grave_b", "stone slab grave", string.byte('n'), false))
-server_addtile(Server, new_tile("wall", "stone brick wall", string.byte('#'), false))
-server_addtile(Server, new_tile("spikes", "iron spikes", string.byte('"')))
+new_tile("floor", "old paved floor", string.byte('.'))
+new_tile("floor drawn", "old paved floor with chalk glyph", string.byte(';'))
+new_tile("portal", "magic portal", string.byte('^'))
+new_tile("door", "wooden door, closed", string.byte('|'), false)
+new_tile("door_open", "wooden door, opened", string.byte('_'))
+new_tile("grave_a", "stone cross grave", string.byte('+'), false)
+new_tile("grave_b", "stone slab grave", string.byte('n'), false)
+new_tile("wall", "stone brick wall", string.byte('#'), false)
+new_tile("spikes", "iron spikes", string.byte('"'))
+
+verbose("Tiles created...")
 
 -- Entrance
-local screen_entrance = new_screen("Entrance", 11, 11, server_gettile(Server, "floor"))
-server_addscreen(Server, "entrance", screen_entrance)
-screen_settile(screen_entrance, 5, 3, server_gettile(Server, "portal"))
-screen_settile(screen_entrance, 5, 7, server_gettile(Server, "portal"))
-screen_setlandon(screen_entrance, 5, 7, "scripts/plop.lua")
-local chalk = new_object("piece of chalk", string.byte('-'))
-object_settag(chalk, "draw", "true")
-object_settag(chalk, "charges", "10")
-screen_addobject(screen_entrance, 8, 7, chalk)
-screen_addobject(screen_entrance, 2, 2, new_object("gem", string.byte('*')))
+new_screen("entrance", "Entrance", 11, 11, "floor")
+screen_settile("entrance", 5, 3, "portal")
+screen_settile("entrance", 5, 7, "portal")
+screen_setlandon("entrance", 5, 7, "scripts/plop.lua")
+
+-- local chalk = new_object("piece of chalk", string.byte('-'))
+-- object_settag(chalk, "draw", "true")
+-- object_settag(chalk, "charges", "10")
+-- screen_addobject(screen_entrance, 8, 7, chalk)
+-- screen_addobject(screen_entrance, 2, 2, new_object("gem", string.byte('*')))
 
 -- Test Room
 local testroom_height = 11;
-local screen_testroom = new_screen("Test Room", 20, testroom_height, server_gettile(Server, "floor"))
-server_addscreen(Server, "testroom", screen_testroom)
-screen_settile(screen_testroom, 3, 3, server_gettile(Server, "portal"))
-screen_setlandon(screen_testroom, 3, 3, "scripts/plop.lua")
+new_screen("testroom", "Test Room", 20, testroom_height, "floor")
+screen_settile("testroom", 3, 3, "portal")
+screen_setlandon("testroom", 3, 3, "scripts/plop.lua")
 for i=0, testroom_height-1, 1 do
-	screen_settile(screen_testroom, 7, i, server_gettile(Server, "wall"))
+	screen_settile("testroom", 7, i, "wall")
 end
-screen_settile(screen_testroom, 7, testroom_height/2, server_gettile(Server, "door"))
-screen_settile(screen_testroom, 13, 5, server_gettile(Server, "spikes"))
-screen_setlandon(screen_testroom, 13, 5, "scripts/spikes.lua")
+screen_settile("testroom", 7, testroom_height/2, "door")
+screen_settile("testroom", 13, 5, "spikes")
+screen_setlandon("testroom", 13, 5, "scripts/spikes.lua")
 
 -- Graveyard
-local screen_graveyard = new_screen("Graveyard", 30, 30, server_gettile(Server, "floor"))
-server_addscreen(Server, "graveyard", screen_graveyard)
+new_screen("graveyard", "Graveyard", 30, 30, "floor")
+
+verbose("PLOP")
 
 -- Player's scripts
-server_addscript(Server, "dance", "scripts/dance.lua")
-server_addscript(Server, "morph", "scripts/morph.lua")
-server_addscript(Server, "name", "scripts/name.lua")
-server_addscript(Server, "open", "scripts/open.lua")
-server_addscript(Server, "close", "scripts/close.lua")
-server_addscript(Server, "damn", "scripts/damn.lua")
-server_addscript(Server, "look", "scripts/look.lua")
-server_addscript(Server, "draw", "scripts/draw.lua")
-server_addscript(Server, "wipe", "scripts/wipe.lua")
+add_script("dance", "scripts/dance.lua")
+add_script("morph", "scripts/morph.lua")
+add_script("name", "scripts/name.lua")
+add_script("open", "scripts/open.lua")
+add_script("close", "scripts/close.lua")
+add_script("damn", "scripts/damn.lua")
+add_script("look", "scripts/look.lua")
+add_script("draw", "scripts/draw.lua")
+add_script("wipe", "scripts/wipe.lua")
 
-server_open(Server, 31337)
-verboseinfo("Lua Init OK")
+open(31337)
+verbose("Lua Init OK")
 
--- spawn.lua
+++ spawn.lua
@@ -1,7 +1,8 @@
 #!/usr/bin/lua
 player_setaspect(Player, string.byte('@'))
 player_setondeath(Player, "scripts/death.lua")
-gauge_setonempty(new_gauge(Player, "life", 3, 3, string.byte('o'), string.byte('.')), "scripts/kill.lua")
+new_gauge(Player, "life", 3, 3, string.byte('o'), string.byte('.'))
+gauge_setonempty(Player, "life", "scripts/kill.lua")
 new_gauge(Player, "xp", 0, 10, string.byte('#'), string.byte('_'), false)
 player_settag(Player, "damned", "false")
-player_spawn(Player, server_getscreen(Server, "entrance"), 5, 3)
+player_spawn(Player, "entrance", 5, 3)
